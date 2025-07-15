450c19eadabf272e33ca30b484446be83eea9f48
Lita Doolan <51329113+LDolanLDolan@users.noreply.github.com>
2025-07-15 11:21:11 +0100

    Update monster.js
    functions prefixed with p

--- monster.js
+++ monster.js
@@ -1,37 +1,39 @@
-function setup() {
-  createCanvas(400, 400);
-  background(200);
+window.currentP5 = new p5((p) => {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+    p.background(200);
 
-  // Body
-  fill(0, 255, 0);
-  rect(150, 150, 100, 100);
+    // Body
+    p.fill(0, 255, 0);
+    p.rect(150, 150, 100, 100);
 
-  // Head
-  rect(150, 50, 100, 100);
+    // Head
+    p.rect(150, 50, 100, 100);
 
-  // Arms
-  rect(110, 150, 40, 20);
-  rect(250, 150, 40, 20);
+    // Arms
+    p.rect(110, 150, 40, 20);
+    p.rect(250, 150, 40, 20);
 
-  // Legs
-  rect(150, 250, 20, 40);
-  rect(230, 250, 20, 40);
+    // Legs
+    p.rect(150, 250, 20, 40);
+    p.rect(230, 250, 20, 40);
 
-  // Eyes
-  fill(255);
-  rect(170, 80, 20, 20);
-  rect(210, 80, 20, 20);
+    // Eyes
+    p.fill(255);
+    p.rect(170, 80, 20, 20);
+    p.rect(210, 80, 20, 20);
 
-  fill(0);
-  rect(178, 88, 4, 4);
-  rect(218, 88, 4, 4);
+    p.fill(0);
+    p.rect(178, 88, 4, 4);
+    p.rect(218, 88, 4, 4);
 
-  // Mouth
-  fill(255, 0, 0);
-  rect(170, 130, 60, 20);
+    // Mouth
+    p.fill(255, 0, 0);
+    p.rect(170, 130, 60, 20);
 
-  // Teeth
-  fill(255);
-  rect(180, 130, 10, 10);
-  rect(210, 130, 10, 10);
-}
+    // Teeth
+    p.fill(255);
+    p.rect(180, 130, 10, 10);
+    p.rect(210, 130, 10, 10);
+  };
+}, 'sketch-holder');
