d1ac25001b9d99333c3c8f0bafd6c77d7477d0ab
Lita Doolan <51329113+LDolanLDolan@users.noreply.github.com>
2025-07-15 12:35:11 +0100

    Update raintwo.js
    format for p frame

--- raintwo.js
+++ raintwo.js
@@ -1,25 +1,29 @@
-function setup() {
-  createCanvas(400, 400);
-  
-}
+window.currentP5 = new p5((p) => {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
 
-function draw() {
-  background(200);
-  let i = 0;
-  while (i<10){
-     fill(0, 0, 255, 10)
-      rect(80*i, height/2-20, random(20), random(20));
-     fill(0, 0, 255, 100)
-      rect(80*i, height/2-20, random(10), random(10));
-    fill(0, 0, 255, 10)
-      rect(80*i, height/2, random(20), random(20));
-     fill(0, 0, 255, 100)
-      rect(80*i, height/2, random(10), random(10));
-    
-    i = i+1;
-  }
-  fill(0, 0, 150, 100);
-  rect(0, height/2, width, height/2)
-  
-}
-  
+  p.draw = function () {
+    p.background(200);
+
+    let i = 0;
+    while (i < 10) {
+      p.fill(0, 0, 255, 10);
+      p.rect(80 * i, p.height / 2 - 20, p.random(20), p.random(20));
+
+      p.fill(0, 0, 255, 100);
+      p.rect(80 * i, p.height / 2 - 20, p.random(10), p.random(10));
+
+      p.fill(0, 0, 255, 10);
+      p.rect(80 * i, p.height / 2, p.random(20), p.random(20));
+
+      p.fill(0, 0, 255, 100);
+      p.rect(80 * i, p.height / 2, p.random(10), p.random(10));
+
+      i = i + 1;
+    }
+
+    p.fill(0, 0, 150, 100);
+    p.rect(0, p.height / 2, p.width, p.height / 2);
+  };
+}, 'sketch-holder');
