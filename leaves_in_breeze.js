2699768b4c89b017f7b01438a2a1f36e8823cb42
Lita Doolan <51329113+LDolanLDolan@users.noreply.github.com>
2025-07-15 11:03:33 +0100

    Update leaves_in_breeze.js
    changed rotation

--- leaves_in_breeze.js
+++ leaves_in_breeze.js
@@ -1,35 +1,40 @@
-let startx = 100;
-let starty = 100;
-let lines = [];
+window.currentP5 = new p5((p) => {
+  let startx = 100;
+  let starty = 100;
+  let lines = [];
 
-class FreshLine {
-  constructor(startx, starty) {
-    this.a = 50; // Hard-coded width of the square
-    this.b = 10; // Hard-coded height of the square
-    this.startx = startx;
-    this.starty = starty;
-  }
+  class FreshLine {
+    constructor(startx, starty) {
+      this.a = 50;
+      this.b = 10;
+      this.startx = startx;
+      this.starty = starty;
+    }
 
-  draw() {
-    fill(random(255));
-    ellipse(this.startx, this.starty, this.a, this.b);
-    rotate(1000)
-  }
-}
-
-function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let newRect = new FreshLine(startx + i * 20, starty + j * 20);
-      lines.push(newRect);
+    draw() {
+      p.push(); // Isolate transformations
+      p.translate(this.startx, this.starty);
+      p.rotate(1000); // Your original value
+      p.fill(p.random(255));
+      p.ellipse(0, 0, this.a, this.b);
+      p.pop();
     }
   }
-}
 
-function draw() {
-  background(220);
-  for (let line of lines) {
-    line.draw();
-  }
-}
+  p.setup = function () {
+    p.createCanvas(400, 400);
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        let newRect = new FreshLine(startx + i * 20, starty + j * 20);
+        lines.push(newRect);
+      }
+    }
+  };
+
+  p.draw = function () {
+    p.background(220);
+    for (let line of lines) {
+      line.draw();
+    }
+  };
+}, 'sketch-holder');
